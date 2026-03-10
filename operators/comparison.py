de53fc97a905ca920e6cb00984f0cf7fa0af4e0e
info-pratyush <info.pratyush01@gmail.com>
2026-03-10 21:29:44 +0530

    add arithmetic, comparison, and logical operator scripts

---
+++ operators/comparison.py
@@ -0,0 +1,12 @@
+print("Comparision Operators In Python")
+
+a = 10
+b = 20
+print("a == b : ", a == b)
+print("a != b : ", a != b)
+print("a > b : ", a > b)
+print("a < b : ", a < b)
+print("a >= b : ", a >= b)
+print("a <= b : ", a <= b)
+
+
---
+++ operators/logical.py
@@ -0,0 +1,12 @@
+print("Logical operators in Python")
+
+a = True
+b = False
+print("a and b:", a and b)
+print("a or b:", a or b)
+print("not a:", not a)
+print("not b:", not b)
+
+age = 20 
+
+print(age > 18 and age < 30)
