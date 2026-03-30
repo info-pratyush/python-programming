104465f6aad630cb55ddb72996f5ab0ad1badc08
info-pratyush <info.pratyush01@gmail.com>
2026-03-30 23:01:57 +0530

    update conditional statement

---
+++ conditional statement/match.py
@@ -0,0 +1,21 @@
+#The match statement selcts one of many code blocks to be executed.
+
+day = int(input("Enter a day number (1-7): "))
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:             #default case if no other case matches
+        print("Invalid day number")        
