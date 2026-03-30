a87b3e20f2a116171ae5a6af31bd8daea76e65ed
info-pratyush <info.pratyush01@gmail.com>
2026-03-30 22:44:19 +0530

    update looping-statements

---
+++ looping-statements/looping-string.py
@@ -0,0 +1,10 @@
+
+for x in "Pratyush":
+    print(x )
+
+#use break to exit the loop when x is "p"
+print("\n")
+for x in "Pratyush":
+    if x == "y":
+        break
+    print(x)
---
+++ looping-statements/pass.py
@@ -0,0 +1,6 @@
+
+for x in range(5):
+    if x == 3:
+        pass  # pass is used as a placeholder for future code
+    else:
+        print(x)
