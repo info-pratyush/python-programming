3869713da3cf2589d34d582e915908ba53233496
info-pratyush <info.pratyush01@gmail.com>
2026-04-17 20:52:37 +0530

    add string loop and functions

---
+++ string/string_functions.py
@@ -0,0 +1,18 @@
+
+name = "pratyush"
+print(name)
+
+# String functions
+print(name.upper())  # PRATYUSH
+print(name.lower())  # pratyush
+print(name.capitalize())  # Pratyush
+print(name.title())  # Pratyush
+print(name.strip())  # pratyush
+print(name.replace("a", "A"))  # prAtyush
+print(name.split("a"))  # ['pr', 'tyush']
+print(name.find("t"))  # 2
+print(name.count("a"))  # 1
+
+
+
+
---
+++ string/string_loop.py
@@ -0,0 +1,16 @@
+ 
+txt = "Python"
+
+for ch in txt:
+    print(ch)
+
+text = "mississippi"
+print(text)
+
+count = 0
+
+for ch in text:
+    if ch == 'i':
+        count += 1    
+
+print("Count of 'i':", count)
