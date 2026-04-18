62e9660298fb7c5687bee8218e08cc8a6381782f
info-pratyush <info.pratyush01@gmail.com>
2026-04-18 20:10:43 +0530

    add list

---
+++ lists/lists.py
@@ -0,0 +1,15 @@
+# List in python is ordered, mutable and allow duplicate elements. It is defined by square brackets [].
+
+a = [10, 20, 30, 40, 50] # creating List
+print(a)
+
+# List can contain different types of data.
+
+b = [10, "Hello", 3.14, True]
+print(b)
+
+# List can also contain other lists.
+
+c = [10, [20, 30], "Python", False]
+print(c)
+
---
+++ lists/lists_index.py
@@ -0,0 +1,11 @@
+# Lists items are indexed by their position in the list, starting with 0. so youn can access any item by referring to its index number.
+
+a = [10, 20, 30, 40, 50]
+print(a[0]) # 10
+print(a[1]) # 20
+print(a[2]) # 30
+
+# Negative indexing = access items from the end of the list.
+
+print(a[-1]) # 50
+print(a[-2]) # 40
