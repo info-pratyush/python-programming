96e14335ae1be8cc24fa47eb52ca19a6b708b687
info-pratyush <info.pratyush01@gmail.com>
2026-04-18 21:47:20 +0530

    update strings

---
+++ string/string_check.py
@@ -0,0 +1,12 @@
+txt = "Microsoft 365"
+print(txt)
+
+# String checks
+print(txt.isalpha())  # False
+print(txt.isdigit())  # False
+print(txt.isalnum())  # False
+print(txt.islower())  # False
+print(txt.isupper())  # False
+print(txt.isspace())  # False
+print(txt.startswith("Microsoft"))  # True
+print(txt.endswith("365"))  # True
---
+++ string/string_slicing.py
@@ -0,0 +1,10 @@
+
+txt = "python programming"
+print(txt)
+
+# String slicing
+print(txt[0:6])  # python
+print(txt[7:18])  # programming
+print(txt[:6])  # python
+print(txt[7:18])  # programming
+print(txt[:])  # python programming
