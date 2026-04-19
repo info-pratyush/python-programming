80c4f55a41e88c127e11d3ef246bdadce3df7ec0
info-pratyush <info.pratyush01@gmail.com>
2026-04-19 17:35:50 +0530

    add dictionary

---
+++ dictionary/dictionary.py
@@ -0,0 +1,16 @@
+# Dictionary in Python stores key-value pairs. It is a collection which is ordered, changeable and does not allow duplicates. Dictionaries are written with curly brackets.
+
+# Creating a Dictionary
+
+my_dict = {
+    "name" : "Pratyush",
+    "age" : 18,
+    "city" : "Ambala"
+}
+
+print(my_dict)
+print("Length of the dictionary: ", len(my_dict))
+
+# Dictionary constructor
+my_dict2 = dict(name="Pratyush", age=18, city="Ambala")
+print(my_dict2)
---
+++ dictionary/dictionary_update.py
@@ -0,0 +1,11 @@
+my_dict = {
+    "name" : "Pratyush",
+    "age" : 18,
+    "city" : "Ambala"
+}
+
+# Updating a Dictionary
+my_dict["age"] = 19
+my_dict["country"] = "India" # Adding a new key-value pair to the dictionary
+
+print(my_dict)
